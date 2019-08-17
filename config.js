80b29f45880a9818cf9c202764291065628f07fe
Luis Lobo Borobia <luislobo@gmail.com>
2019-08-17 12:27:00 -0500

    updated social media accounts

--- config.js
+++ config.js
@@ -2,14 +2,14 @@ module.exports = {
   // the name of your website
   title: 'Lobo',
   // the description of the website (eg. what shows on Google)
-  description: "Lobo's blog",
+  description: "Luis Lobo's blog",
   // a short bio shown at the bottom of your blog posts
   // It should complete the sentence: Written by Lobo ...
   shortBio: '',
   // a longer bio showing on the landing page of the blog
   bio: `Father, Technology Advisor, Coder, Inline Skater, Amateur Photographer https://about.me/luislobo.`,
   author: 'Lobo',
-  githubUrl: 'https://github.com/',
+  githubUrl: 'https://github.com/luislobo',
   // replace this by the url where your website will be published
   siteUrl: 'https://lobo-blog.netlify.com',
   social: {
@@ -17,8 +17,8 @@ module.exports = {
     twitter: 'luislobo',
     medium: '@luislobo',
     facebook: '',
-    github: '',
-    linkedin: '',
-    instagram: '',
+    github: 'luislobo',
+    linkedin: 'llobo',
+    instagram: 'luislobo',
   },
 }
