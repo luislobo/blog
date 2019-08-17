7bb2ea7878068276f732d19c76f0fc2ce4805c20
Luis Lobo Borobia <luislobo@gmail.com>
2019-08-17 12:25:41 -0500

    update config to use blog's URL

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   author: 'Lobo',
   githubUrl: 'https://github.com/',
   // replace this by the url where your website will be published
-  siteUrl: 'http://localhost:8000',
+  siteUrl: 'https://lobo-blog.netlify.com',
   social: {
     // leave the social media you do not want to appear as empty strings
     twitter: 'luislobo',
